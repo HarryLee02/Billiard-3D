37e9c96d7250573dea1d3b4ff2d748e61a49d58e
harrylee02 <ngochieu0211.study@gmail.com>
2024-04-23 14:05:08 +0700

    Push gameplay

---
+++ Billiard/Assets/Scripts/GameManager.cs
@@ -0,0 +1,208 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    enum CurrentPlayer
+    {
+        Player1,
+        Player2
+    }
+
+    CurrentPlayer currentPlayer;
+    bool isWinningShotForPlayer1 = false;
+    bool isWinningShotForPlayer2 = false;
+    int player1BallsRemaining = 7;
+    int player2BallsRemaining = 7;
+
+    [SerializeField] TextMeshProUGUI player1BallsText;
+    [SerializeField] TextMeshProUGUI player2BallsText;
+    [SerializeField] TextMeshProUGUI currentTurnText;
+    [SerializeField] TextMeshProUGUI messageText;
+
+    [SerializeField] GameObject restartButton;
+
+    [SerializeField] Transform headPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlayer = CurrentPlayer.Player1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartTheGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    bool Scratch()
+    {
+        if (currentPlayer == CurrentPlayer.Player1)
+        {
+            if (isWinningShotForPlayer1)
+            {
+                ScratchOnWinningShot("Player 1");
+                return true;
+            }
+        }
+        else
+        {
+            if (isWinningShotForPlayer2)
+            {
+                ScratchOnWinningShot("Player 2");
+                return true;
+            }
+        }   
+        NextPlayerTurn();
+        return false;
+    }
+
+    void EarlyEightBall()
+    {
+        if (currentPlayer == CurrentPlayer.Player1)
+        {
+            Lose("Player 1 hit the 8 ball too early!");
+        }
+        else
+        {
+            Lose("Player 2 hit the 8 ball too early!");
+        }
+    }
+
+    void ScratchOnWinningShot(string player)
+    {
+        Lose (player + " scratched on their final shot!");
+    }
+
+    void NoMoreBall(CurrentPlayer player)
+    {
+        if (player == CurrentPlayer.Player1)
+        {
+            isWinningShotForPlayer1 = true;
+        }
+        else
+        {
+            isWinningShotForPlayer2 = true;
+        }
+    }
+
+    bool CheckBall(Ball ball)
+    {
+        if (ball.IsCueBall())
+        {
+            if (Scratch())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (ball.IsEightBall())
+        {
+            if (currentPlayer == CurrentPlayer.Player1)
+            {
+                if (isWinningShotForPlayer1)
+                {
+                    Win("Player 1");
+                    return true;
+                }
+            }
+            else
+            {
+                if (isWinningShotForPlayer2)
+                {
+                    Win("Player 2");
+                    return true;
+                }
+            }
+            EarlyEightBall();
+        }
+        else
+        {
+            if (ball.IsRedBall())
+            {
+                player1BallsRemaining--;
+                player1BallsText.text = "Player 1 Balls Remaining: " + player1BallsRemaining;
+                if (player1BallsRemaining <= 0)
+                {
+                    isWinningShotForPlayer1 = true;
+                }
+                if (currentPlayer != CurrentPlayer.Player1)
+                {
+                    NextPlayerTurn();
+                }
+            }
+            else
+            {
+                player2BallsRemaining--;
+                player2BallsText.text = "Player 2 Balls Remaining: " + player2BallsRemaining;
+                if (player2BallsRemaining <= 0)
+                {
+                    isWinningShotForPlayer2 = true;
+                }
+                if (currentPlayer != CurrentPlayer.Player2)
+                {
+                    NextPlayerTurn();
+                }
+            }
+        }
+        return true;
+    }
+
+    void Lose (string message)
+    {
+        messageText.gameObject.SetActive(true);
+        messageText.text = message;
+        restartButton.SetActive(true);
+    }
+
+    void Win (string player)
+    {
+        messageText.gameObject.SetActive(true);
+        messageText.text = player + " has won!";
+        restartButton.SetActive(true);
+    }
+
+    void NextPlayerTurn()
+    {
+        if (currentPlayer == CurrentPlayer.Player1)
+        {
+            currentPlayer = CurrentPlayer.Player2;
+            currentTurnText.text = "Player 2's Turn";
+        }
+        else
+        {
+            currentPlayer = CurrentPlayer.Player1;
+            currentTurnText.text = "Player 1's Turn";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            if (CheckBall(other.gameObject.GetComponent<Ball>()))
+            {
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                other.gameObject.transform.position = headPosition.position;
+                other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                other.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;  
+            }
+        }   
+    }
+}
---
+++ Billiard/Assets/Scripts/GameSetup.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{   
+    int redBallsRemaining = 7;
+    int blueBallsRemaining = 7;
+    float ballRadius;
+    float ballDiameter;
+
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] Transform cueBallPosition;
+    [SerializeField] Transform headBallPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRadius = ballPrefab.GetComponent<SphereCollider>().radius * 100f;
+        ballDiameter = ballRadius * 2f;
+        PlaceAllBalls();
+    }
+    
+    void PlaceAllBalls()
+    {
+        PlaceCueBall();
+        PlaceRandomBalls();
+    }
+
+    void PlaceCueBall()
+    {
+        GameObject ball = Instantiate(ballPrefab, cueBallPosition.position, Quaternion.identity);
+        ball.GetComponent<Ball>().MakeCueBall();
+    }
+
+    void PlaceEightBall(Vector3 position)
+    {
+        GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+        ball.GetComponent<Ball>().MakeEightBall();
+    }
+
+    void PlaceRandomBalls()
+    {
+        int NumInThisRow = 1;
+        int rand;
+        Vector3 FirstInRowPosition = headBallPosition.position;
+        Vector3 CurrentPosition = FirstInRowPosition;
+
+        void PlaceRedBall(Vector3 position)
+        {
+            GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+            ball.GetComponent<Ball>().BallSetup(true);
+            redBallsRemaining--;
+        }
+
+        void PlaceBlueBall(Vector3 position)
+        {
+            GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
+            ball.GetComponent<Ball>().BallSetup(false);
+            blueBallsRemaining--;
+        }
+
+        // Outer loop are the 5 rows
+        for (int i = 0; i < 5; i++)
+        {   
+            //Inner loop is the number of balls in each row
+            for (int j = 0; j < NumInThisRow; j++)
+            {   
+                // Check to see thats a middle spot for the 8 ball
+                if (i == 2 && j == 1)
+                {
+                    PlaceEightBall(CurrentPosition);
+                }
+                // If there are still red and blue balls remaining, randomly place one
+                else if (redBallsRemaining > 0 && blueBallsRemaining > 0)
+                {
+                    rand = Random.Range(0, 2);
+                    if (rand == 0)
+                    {
+                        PlaceRedBall(CurrentPosition);
+                    }
+                    else
+                    {
+                        PlaceBlueBall(CurrentPosition);
+                    }
+                }
+                // If there are only red balls remaining, place a red ball
+                else if (redBallsRemaining > 0)
+                {
+                    PlaceRedBall(CurrentPosition);
+                }
+                // If there are only blue balls remaining, place a blue ball
+                else if (blueBallsRemaining > 0)
+                {
+                    PlaceBlueBall(CurrentPosition);
+                }  
+                // Move to the next position to the right
+                CurrentPosition += new Vector3(1, 0, 0).normalized * ballDiameter;
+            }
+            // Once the row is complete, move to the next row
+            FirstInRowPosition += Vector3.back * (Mathf.Sqrt(3) * ballRadius) + Vector3.left * ballRadius;
+            CurrentPosition = FirstInRowPosition;
+            NumInThisRow++;
+        }
+    }
+}
