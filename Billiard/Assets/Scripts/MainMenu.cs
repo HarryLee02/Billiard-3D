20e794ebda6ed9fc1c3680f15df51fa14ca90576
Howf1706 <121178605+Howf1706@users.noreply.github.com>
2024-04-27 16:01:03 +0700

    Move from demo to new branch
    assets/bg, assets/scenes, scripts

---
+++ Billiard/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void EnterGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ReturnMainMenu() {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame() {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
